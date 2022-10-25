b7f772939f78e02781ca9ba83b5f23c155c05b12
nuradil7704 <113079748+nuradil7704@users.noreply.github.com>
2022-10-25 23:31:48 +0600

    Add files via upload

---
+++ DZ4-Nuradil-18-01/app.js
@@ -0,0 +1,15 @@
+const btn = document.querySelector(".btn")
+
+btn.addEventListener("click", ()=>{
+    const request= new XMLHttpRequest();
+    request.open("GET", "data.json");
+    request.setRequestHeader("Content-type", "application/json");
+    request.send();
+    request.addEventListener("load", ()=>{
+        console.log(JSON.parse(request.response));
+        const data= (JSON.parse(request.response))
+        document.querySelector(".number").innerHTML = data.number;
+        document.querySelector(".login").innerHTML = data.login;
+        console.log(request.response);
+    })
+});
